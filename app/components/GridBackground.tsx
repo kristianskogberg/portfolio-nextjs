3848bc5ac62b8c694cee8d8e32f4079ff89dc50b
kristianskogberg <k.skogberg98@gmail.com>
2024-07-23 13:39:26 +0300

    fix screen height bg

--- app/components/GridBackground.tsx
+++ app/components/GridBackground.tsx
@@ -4,6 +4,6 @@ type Props = {};
 
 export default function GridBackground({}: Props) {
   return (
-    <div className="-z-50 fixed inset-0 h-full w-full bg-dark bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"></div>
+    <div className="-z-50 fixed inset-0 h-full w-full min-h-screen bg-dark bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"></div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.variable} ${fira_code.variable}`}>
-      <body className="h-dvh flex flex-col w-full">
+      <body>
         <Navbar />
         <GridBackground />
         {children}
